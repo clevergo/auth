3b1336bf82731156d9dbec241ed0248b560316f4
razonyang <razonyang@gmail.com>
2020-05-16 02:32:49 +0800

    Fix golint style mistakes

--- middleware.go
+++ middleware.go
@@ -12,13 +12,16 @@ import (
 
 type contextKey int
 
+// IdentityKey is the context key of identity.
 const IdentityKey contextKey = 1
 
+// GetIdentity retrieves the identity from context.
 func GetIdentity(ctx context.Context) Identity {
 	identity, _ := ctx.Value(IdentityKey).(Identity)
 	return identity
 }
 
+// NewMiddleware returns a middleware with the given authenticator.
 func NewMiddleware(authenticator Authenticator) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
